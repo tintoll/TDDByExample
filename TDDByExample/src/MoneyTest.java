0a222717d436cd239f8c400b7f4fffec8d77f052
tintoll <belselios@gmail.com>
2017-04-13 00:40:04 +0900

    13장 까지 실습 추가

--- TDDByExample/src/MoneyTest.java
+++ TDDByExample/src/MoneyTest.java
@@ -17,54 +17,112 @@ public class MoneyTest extends TestCase {
 	public void testEquality() {
 		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
 		assertFalse(Money.dollar(6).equals(Money.dollar(5)));
-		
-		assertTrue(Money.franc(5).equals(new Franc(5)));
-		assertFalse(Money.franc(6).equals(new Franc(5)));
-		
 		assertFalse(Money.franc(5).equals(Money.dollar(5)));
 	}
+	
+	public void testCurrency() {
+		assertEquals("USD", Money.dollar(1).currency());
+		assertEquals("CHF", Money.franc(1).currency());
+	}
+	
+	public void testSimpleAddition() {
+		
+		Money five = Money.dollar(5);
+		Expression sum = five.plus(five);
+		Bank bank = new Bank();
+		Money reduced = bank.reduce(sum,"USD");
+		assertEquals(Money.dollar(10), reduced);
+	}
+	
+	public void testPlusReturnSum() {
+		Money five = Money.dollar(5);
+		Expression result = five.plus(five);
+		Sum sum = (Sum)result;
+		assertEquals(five, sum.augend);
+		assertEquals(five, sum.addend);
+	}
+	
+	public void testReduceSum() {
+		Expression sum = new Sum(Money.dollar(3), Money.dollar(4));
+		Bank bank = new Bank();
+		Money result = bank.reduce(sum, "USD");
+		assertEquals(Money.dollar(7), result);
+	}
+	
+	public void testReduceMoney() {
+		Bank bank = new Bank();
+		Money result = bank.reduce(Money.dollar(1), "USD");
+		assertEquals(Money.dollar(1), result);
+	}
+	
 
 }
-abstract class Money {
+class Money implements Expression{
 	protected int amount;
+	protected String currency;
 	
 	public boolean equals(Object object) {
 		Money money = (Money)object;
-		return amount == money.amount && getClass().equals(money.getClass());
+		return amount == money.amount && currency().equals(money.currency());
+	}
+	Expression plus(Money addend) {
+		return new Sum(this, addend);
+	}
+	public Money(int amount, String currency) {
+		this.amount = amount;
+		this.currency = currency;
 	}
 
 	static Money dollar(int amount) {
-		return new Dollar(amount);
+		return new Money(amount, "USD");
 	}
 	static Money franc(int amount) {
-		return new Franc(amount);
-	}
-	
-	abstract Money times(int multiplier);
-}
-
-class Dollar extends Money{
-
-	Dollar(int amount) {
-		this.amount = amount;
+		return new Money(amount, "CHF");
 	}
 
 	Money times(int multiplier) {
-		return new Dollar(amount * multiplier);
+		return new Money(amount * multiplier, currency);
+	}
+	
+	String currency() {
+		return currency;
 	}
 	
 	
+	public Money reduce(String to) {
+		return this;
+	}
+	
+	public String toString() {
+		return amount + " "+currency;
+	}
+}
+
+interface Expression {
+
+	Money reduce(String to);
 	
 }
-class Franc extends Money{
+class Bank {
 
-	Franc(int amount) {
-		this.amount = amount;
-	}
-
-	Money times(int multiplier) {
-		return new Franc(amount * multiplier);
+	public Money reduce(Expression source, String to) {
+		return source.reduce(to);
 	}
 	
+}
+
+class Sum implements Expression{
+	Money augend;
+	Money addend;
+	
+	public Sum(Money augend, Money addend) {
+		this.augend = augend;
+		this.addend = addend;
+	}
+
+	public Money reduce(String to) {
+		int amount = augend.amount + addend.amount;
+		return new Money(amount, to);
+	}
 	
 }
