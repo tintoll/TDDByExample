c63d0d20e15e01c03e8f54ebe4aa905606a0bfd8
tintoll <belselios@gmail.com>
2017-04-26 00:41:25 +0900

    테스트코드 완료

--- python/xunit.py
+++ python/xunit.py
@@ -1,30 +1,86 @@
 class TestCase:
     def __init__(self, name):
         self.name = name
-    def run(self):
+    def run(self,result):
+        result.testStarted()
         self.setUp()
-        method=getattr(self,self.name)
-        method()
+        try:
+            method=getattr(self,self.name)
+            method()
+        except:
+            result.testFailed()
+        self.tearDown()
+        return result
     def setUp(self):
+        pass
+    def tearDown(self):
         pass
 
 class WasRun(TestCase):
     def testMethod(self):
-        self.wasRun = 1
+        self.log=self.log+"testMethod "
+    def testBrokenMethod(self):
+        raise Exception
     def setUp(self):
-        self.wasRun=None
-        self.wasSetUp=1
+        self.log="setUp "
+    def tearDown(self):
+        self.log=self.log+"tearDown "
+
+class TestResult:
+    def __init__(self):
+        self.runCount=0
+        self.failureCount=0
+    def testStarted(self):
+        self.runCount=self.runCount+1
+    def testFailed(self):
+        self.failureCount = self.failureCount+1
+    def summary(self):
+        return "%d run, %d failed"%(self.runCount,self.failureCount)
+
+class TestSuite:
+    def __init__(self):
+        self.tests = []
+    def add(self,test):
+        self.tests.append(test)
+    def run(self,result):
+        for test in self.tests:
+            test.run(result)
 
 
 class TestCaseTest(TestCase):
     def setUp(self):
-        self.test=WasRun("testMethod")
-    def testRunning(self):
-        self.test.run()
-        assert (self.test.wasRun)
-    def testSetUp(self):
-        self.test.run()
-        assert (self.test.wasSetUp)
+        self.result=TestResult()
 
-TestCaseTest("testRunning").run()
-TestCaseTest("testSetUp").run()
+    def testTemplateMethod(self):
+        test=WasRun("testMethod")
+        test.run(self.result)
+        assert ("setUp testMethod tearDown "==test.log)
+    def testResult(self):
+        test=WasRun("testMethod")
+        test.run(self.result)
+        assert ("1 run, 0 failed"==self.result.summary())
+    def testFailedResult(self):
+        test=WasRun("testBrokenMethod")
+        test.run(self.result)
+        assert ("1 run, 1 failed"==self.result.summary())
+    def testFailedResultFormatting(self):
+        self.result.testStarted()
+        self.result.testFailed()
+        assert ("1 run, 1 failed"==self.result.summary())
+    def testSuite(self):
+        suite=TestSuite()
+        suite.add(WasRun("testMethod"))
+        suite.add(WasRun("testBrokenMethod"))
+        suite.run(self.result)
+        assert ("2 run, 1 failed"==self.result.summary())
+
+
+suite=TestSuite()
+suite.add(TestCaseTest("testTemplateMethod"))
+suite.add(TestCaseTest("testResult"))
+suite.add(TestCaseTest("testFailedResultFormatting"))
+suite.add(TestCaseTest("testFailedResult"))
+suite.add(TestCaseTest("testSuite"))
+result=TestResult()
+suite.run(result)
+print(result.summary())
